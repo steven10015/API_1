97ff4bdfc5a0c8857ae56762932a64b50a8a10c3
Alfredo Castaneda <70920990+casalf724@users.noreply.github.com>
2023-12-19 20:19:07 +0000

    Had to save plot as png file

--- src/app.py
+++ src/app.py
@@ -5,6 +5,11 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from dotenv import load_dotenv
 
+
+#import matplotlib
+#matplotlib.use('TkAgg')
+
+
 # Load the .env file variables
 load_dotenv()
 
@@ -12,19 +17,19 @@ load_dotenv()
 client_id = os.environ.get("CLIENT_ID")
 client_secret = os.environ.get("CLIENT_SECRET")
 
-if not client_id or not client_secret:
-    raise Exception("CLIENT_ID and CLIENT_SECRET must be set in the environment variables")
+#if not client_id or not client_secret:
+#    raise Exception("CLIENT_ID and CLIENT_SECRET must be set in the environment variables")
 
 # Spotify Client Credentials Authentication
 credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 spotify = spotipy.Spotify(client_credentials_manager=credentials_manager)
 
 # Artist URI
-artist_id = '58zz0VTpGNHn7MGTlW2cxQ'
+artist_Id = '58zz0VTpGNHn7MGTlW2cxQ'
 
 # Fetching the top tracks of the artist
 try:
-    results = spotify.artist_top_tracks(artist_id)
+    results = spotify.artist_top_tracks(artist_Id)
 
     # Extract track data - Transform to Pandas DataFrame
     tracks_data = [{
@@ -35,9 +40,9 @@ try:
 
     # Create a DataFrame and sort by popularity
     df_tracks = pd.DataFrame(tracks_data)
-    df_sorted = df_tracks.sort_values(by='Popularity') #sort the songs by increasing popularity 
-
-    #print(df_sorted.tail(3)) # display the resulting top 3
+    df_sorted = df_tracks.sort_values(by='Popularity') #sort the songs by increasing popularity
+    # display the resulting top 3
+    print(df_sorted.tail(3))
 
     # Plotting
     plt.figure(figsize=(10, 6))
@@ -46,6 +51,9 @@ try:
     plt.xlabel('Duration (Minutes)')
     plt.ylabel('Popularity')
     plt.grid(True)
+
+    # Save the plot as an image file
+    plt.savefig('scatter_plot.png')
     plt.show()
 
 except spotipy.exceptions.SpotifyException as e:
