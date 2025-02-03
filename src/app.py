2201af2b984aaeb7fd720720746dd0dc7e7a89dd
Paula3228 <p.orjuelavera@gmail.com>
2025-02-03 01:18:31 +0000

    Main Integrándose a una API usando Python

--- src/app.py
+++ src/app.py
@@ -1,7 +1,39 @@
 import os
+import spotipy
 import pandas as pd
 import seaborn as sns
 from dotenv import load_dotenv
 
 # load the .env file variables
 load_dotenv()
+client_id = os.environ.get('CLIENT_ID')
+client_secret = os.environ.get('CLIENT_SECRET')
+
+
+
+from spotipy.oauth2 import SpotifyClientCredentials
+
+con = spotipy.Spotify(auth_manager = SpotifyClientCredentials(client_id = client_id,
+                                                              client_secret = client_secret))
+
+
+artist_id = "3TVXtAsR1Inumwj472S9r4"
+response = con.artist_top_tracks("3TVXtAsR1Inumwj472S9r4")
+if response:
+    # We keep the "tracks" object of the answer
+    tracks = response["tracks"]
+    # We select, for each song, the data we are interested in and discard the rest
+    tracks = [{k: (v/(1000*60))%60 if k == "duration_ms" else v for k, v in track.items() if k in ["name", "popularity", "duration_ms"]} for track in tracks]
+
+
+
+tracks_df = pd.DataFrame.from_records(tracks)
+tracks_df.sort_values(["popularity"], inplace = True)
+
+print(tracks_df.head(3))
+
+
+
+scatter_plot = sns.scatterplot(data = tracks_df, x = "popularity", y = "duration_ms")
+fig = scatter_plot.get_figure()
+fig.savefig("scatter_plot.png")
