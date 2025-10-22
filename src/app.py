bcd93438a14fb9193511fcb797026f10c2cb37c5
steven10015 <stevensebastianhq@gmail.com>
2025-10-22 15:52:52 +0000

    Primeros 5 ejercicios

--- src/app.py
+++ src/app.py
@@ -8,3 +8,25 @@ load_dotenv()
 
 
 # Spotify API credentials
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
+
+# Importar spotipy
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+spotify = spotipy.Spotify(auth_manager=auth_manager)
+
+# Seleccionar artista
+id_artista = "7Ln80lUS6He07XvHI8qqHH"
+# Seleccionar tracks
+canciones_top = spotify.artist_top_tracks(id_artista)
+
+songs = []
+for track in results['tracks']:
+    songs.append({
+        'name': track['name'],
+        'popularity': track['popularity'],
+        'duration_min': track['duration_ms'] / 60000
+    })
